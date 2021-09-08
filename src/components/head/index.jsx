ec5f1047f4cdec4110fe3decbec3d6b73b727521
yangchuting <1198854050@qq.com>
2021-09-08 15:46:39 +0800

    some styles

---
+++ src/components/head/index.jsx
@@ -0,0 +1,17 @@
+// 
+import './index.module.scss'
+
+// import styles from './index.module.css'
+
+export default  function Head(){
+    return (<div className="head">
+        <div className='head-cont'>
+            <p>title</p>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>)
+}

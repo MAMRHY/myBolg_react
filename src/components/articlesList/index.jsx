d494e7169cc514ac7cdbd28402724f3846054fe2
yangchuting <1198854050@qq.com>
2022-02-08 16:41:13 +0800

    解决控制台的useEffect警告

--- src/components/articlesList/index.jsx
+++ src/components/articlesList/index.jsx
@@ -4,18 +4,22 @@ import  styles from './index.module.scss'
 function ArticlesList(props){
    
 
-    useEffect(()=>{
+    useEffect((props)=>{
         console.log('zujianprops',props)
     },[props.list])
 
     const filter = (time)=>{
         return time.split('T')[0]
+    }
+
+    const toSee = (id)=>{  //文章id
 
     }
+
     return (<div>
-        <div className={styles.cont1}>
+        <div className={styles.cont1} >
             {props.list.map((item,index)=>{
-                return ( <div className={styles.contDiv} key={index}>
+                return ( <div className={styles.contDiv} key={index} onClick={toSee(item.id)}>
                     <p className={styles.pLeft}> <span className={styles.dot}></span> {item.title}</p>
                     <p className={styles.pRight}>{filter(item.add_time)}</p>
                 </div>)

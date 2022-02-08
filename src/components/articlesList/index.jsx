69c4f82fa758fe600d4417bfe65df085ab28e600
yangchuting <1198854050@qq.com>
2022-02-08 16:50:53 +0800

    这才是解决控制台useEffect报错

--- src/components/articlesList/index.jsx
+++ src/components/articlesList/index.jsx
@@ -4,22 +4,19 @@ import  styles from './index.module.scss'
 function ArticlesList(props){
    
 
-    useEffect((props)=>{
+    useEffect(()=>{
         console.log('zujianprops',props)
-    },[props.list])
+    },[props])
 
     const filter = (time)=>{
         return time.split('T')[0]
     }
 
-    const toSee = (id)=>{  //文章id
-
-    }
 
     return (<div>
         <div className={styles.cont1} >
             {props.list.map((item,index)=>{
-                return ( <div className={styles.contDiv} key={index} onClick={toSee(item.id)}>
+                return ( <div className={styles.contDiv} key={index} >
                     <p className={styles.pLeft}> <span className={styles.dot}></span> {item.title}</p>
                     <p className={styles.pRight}>{filter(item.add_time)}</p>
                 </div>)

d8f919c32b90e27c808514ce568b6cd610265497
yangchuting <1198854050@qq.com>
2022-02-09 18:16:59 +0800

    完成评论模板样式

--- src/views/articles/index.jsx
+++ src/views/articles/index.jsx
@@ -1,14 +1,60 @@
 import { useEffect, useState } from "react"
 import { getArticleInfoApi } from "../../server/articles"
-import { Result } from 'antd';
+import { Result, Comment, Avatar, Form, Button, List, Input ,Tooltip } from 'antd';
 import style from './index.module.scss';
 import ReactMarkdown from 'react-markdown'
+
+import moment from 'moment';
+
+const { TextArea } = Input;
+const CommentList = ({ comments }) => (
+    <List
+      dataSource={comments}
+      header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+      itemLayout="horizontal"
+      renderItem={props => <Comment {...props} />}
+    />
+  );
+  
+  const Editor = ({ onChange, onSubmit, submitting, value }) => (
+    <>
+      <Form.Item>
+        <TextArea rows={4} onChange={onChange} value={value} />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+          Add Comment
+        </Button>
+      </Form.Item>
+    </>
+  );
 
 export default function Articles({match},props){
     const [info, setInfo] = useState({})
     const [errshow, setShow] = useState(false)
+    const [comments,setComments] = useState([])
+    const [submitting,setSubmitting] = useState(false)
+    const [value,setValue] = useState('')
+
 
     useEffect(()=>{
+        setComments([{
+            actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+            author: 'Han Solo',
+            avatar: 'https://joeschmoe.io/api/v1/random',
+            content: (
+              <p>
+                We supply a series of design principles, practical patterns and high quality design
+                resources (Sketch and Axure), to help people create their product prototypes beautifully and
+                efficiently.
+              </p>
+            ),
+            datetime: (
+              <Tooltip title={moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')}>
+                <span>{moment().subtract(1, 'days').fromNow()}</span>
+              </Tooltip>
+            ),
+          }])
 
         getArticleInfoApi(match.params.id).then(res=>{
             if(res.code === 200){
@@ -25,6 +71,16 @@ export default function Articles({match},props){
     //     return time.split('T')[0]
     // }
 
+    const handleSubmit = ()=>{}
+ 
+    const handleChange = e => {
+      this.setState({
+        value: e.target.value,
+      });
+    };
+
+
+
     return ( <div>
         <div style={{display: errshow?'block':'none'}} >
             <Result
@@ -38,8 +94,26 @@ export default function Articles({match},props){
             <p className={style.time}>作者©{info.name}  | </p>
             {/* TODO {filter(info.add_time)} 时间没显示出来，报错 */}
             <hr />
-            <div className={style.mdBox}><ReactMarkdown>{info.md_content}</ReactMarkdown></div>
+            <div className={style.mdBox}>
+                <ReactMarkdown>{info.md_content}</ReactMarkdown>
+            </div>
+            <hr />
+
+            {comments.length > 0 && <CommentList comments={comments} />}
+            <Comment
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
+            content={
+                <Editor
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                submitting={submitting}
+                value={value}
+                />
+            }
+            />
+
         </div>
+
     </div> )
 
 }
